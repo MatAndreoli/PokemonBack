007f042614fa45ef881b05342ee19bf1e0b52476
MatAndreoli <andreoli.2003@gmail.com>
2022-11-19 11:10:25 -0300

    update python script to generate next tag

--- get_last_docker_img.py
+++ get_last_docker_img.py
@@ -7,6 +7,10 @@ payload = ""
 response = requests.request("GET", url, data=payload)
 json_response = json.loads(response.text)
 
+first = json_response["results"][1]["name"].split('.')[0]
+second = int(json_response["results"][1]["name"].split('.')[1]) + 1
+next_version = f'{first}.{second}'
+
 with open(".env", "w") as f:
-    f.write("NEXT_VERSION=" + str(float(json_response["results"][1]["name"]) + 0.1))
+    f.write("NEXT_VERSION=" + next_version)
     f.close
