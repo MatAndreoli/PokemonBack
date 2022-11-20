45217a5d418c1865dc1aa632541e116f4c7b97df
MatAndreoli <andreoli.2003@gmail.com>
2022-11-20 10:39:01 -0300

    Update git hooks

--- enable-pre-push.sh
+++ enable-pre-push.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-rm $(pwd)/.git/hooks/pre-push
-rm $(pwd)/.git/hooks/post-commit
-
 ln -s $(pwd)/custom_hooks/pre-push.sh .git/hooks/pre-push
-ln -s $(pwd)/custom_hooks/post-commit.sh .git/hooks/post-commit
